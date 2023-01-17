cd6bad49df62241d9eabb85b0c9f99edccb3cc7a
jge162 <escobarjeremy.com>
2023-01-17 00:58:23 -0800

    changed code

--- questions.py
+++ questions.py
@@ -11,16 +11,30 @@ class Parent:
         print("Parent class initialized")
 
     def momndad(self, mom, dad):  # want to add input for mom and dads name now
+
         mom = input("Enter moms name: ")  # worked
         dad = input("Enter dads name: ")
         Parent.mom = mom
         Parent.dad = dad
 
-    def setDadnMom(self, momage, dadage):
-        momage = input("Enter dads age: ")
-        dadage = input("Enter moms age: ")
-        Parent.count = momage
-        Parent.count2 = dadage
+    def setDadnMom(self, momAge, dadAge):
+
+        while 1:
+            momAge = int(input("Enter moms age: "))
+            if momAge < 18:
+                print("Too young to get married mom ")
+            elif momAge >= 18:
+                print("Perfect age mom, lets go. ")
+            dadAge = int(input("Enter dads age: "))
+            if dadAge < 18:
+                print("Too young to get married dad ")
+                break
+            elif dadAge >= 18:
+                print("Perfect age dad, lets go. ")
+                break
+
+        Parent.count = momAge
+        Parent.count2 = dadAge
 
     def showDadnMom(self):
         print("Dads age is: ", Parent.count)
@@ -30,6 +44,7 @@ class Parent:
 class Child(Parent):  # child of Parent
     def __init__(self, childName):
         super().__init__()
+        # childName = input(" Enter child's name: ")
         self.childName = childName
         print("Child class initialized")
 
@@ -37,12 +52,12 @@ class Child(Parent):  # child of Parent
 c = Child("John")  # child inherits moms n dads stuff
 # c.setCount(50)  # set counter value LOL
 # c.showMeCount()
-c.setDadnMom(10, 15)  # started as 10 and 15 but updated after user input
+c.setDadnMom(30, 30)  # started as 10 and 15 but updated after user input
 c.momndad('Jane', 'Jack')  # started as Jane and Jack but updated after user input
 
 print("================================")
 print("Mom's name is " + str(c.mom) + "\n" + "Dad's name is " + str(c.dad) + "\n" + "Their child's mame is " + str(
     c.childName))
 print("================================")
-print("Mom is " + c.count + " years old \n" + "& Dad is " + c.count2 + " years old")
+print("Mom is " + str(c.count) + " years old \n" + "& Dad is " + str(c.count2) + " years old")
 print("================================")
