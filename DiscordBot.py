59f368417c89defcbbab04fd429260045300a40b
jge162 <escobarjeremy.com>
2023-01-20 02:57:45 -0800

    updated

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,5 +1,7 @@
 import discord
 import random
+
+from discord import client
 from discord.ext import commands
 
 
@@ -10,10 +12,21 @@ class MyClient(discord.Client):
         print('Bot is Ready')
 
     @commands.command()
-    async def random(self, ctx):
+    async def random(self, ctx, username=None):
         items = ["item 1", "item 2", "item 3"]
+
         await ctx.send(random.choice(items))
         return
+
+    """
+    @commands.has_permissions(administrator=True)
+    @client.command(aliases=["purge", "delete", "clear"])
+    async def sell(self, ctx, amount: int):
+        await ctx.channel.purge(limit=amount + 1)
+        embed = discord.Embed(description=f"**Successfully Sold {amount} message(s) in {ctx.channel}!**",
+                              color=discord.Colour.green())
+        await ctx.send(embed=embed, delete_after=5)
+     """
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -41,8 +54,26 @@ class MyClient(discord.Client):
             await message.channel.send('This can be used anywhre')
             return
 
+        if message.author == client.user:
+            return
+
+        if message.content == 'cool':
+            await message.add_reaction('\U0001F92C')
+
+        @client.event
+        async def on_message_edit(before, after):
+            await before.channel.send(
+                f'{before.author} edited a message\n'
+                f'Before: {before.content}\n'
+                f'After: {after.content}'
+            )
+
+        @client.event
+        async def on_reaction_add(reaction, user):
+            await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = MyClient(intents=intents, command_prefix='!')
+client = MyClient(intents=intents, command_prefix='/')
 client.run('')
