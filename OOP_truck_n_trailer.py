ad9f8e0a1f48316e95de0a07aee8e1bffc6f2789
jge162 <escobarjeremy.com>
2023-01-23 01:03:08 -0800

    tweaked to take inputs

--- OOP_truck_n_trailer.py
+++ OOP_truck_n_trailer.py
@@ -3,20 +3,22 @@
 
 class Truck:
     def __init__(self, tractor):
+        tractor = input("Enter type of tractor: ")
         self.tractor = tractor
 
 
 class Trailer:
     def __init__(self, load):
+        load = input("Enter type of trailer: ")
         self.load = load
 
 
-semi = Truck("Ken worth")
-tail = Trailer("box trailer")
+semi = Truck("Ken worth")  # placeholder
+tail = Trailer("box trailer")  # placeholder
 #  what type of variable are these objects?
 
-print(semi.tractor)
-print(tail.load)
+print("The type of semi is a " + semi.tractor)
+print("The type trailer is a " + tail.load)
 
 print(isinstance(semi, Truck))
 print(isinstance(tail, Trailer))
