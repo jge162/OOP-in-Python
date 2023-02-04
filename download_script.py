96539c47d2f63a53eddfdb9adf2a17c75b94facd
Jermyiah <31228460+jge162@users.noreply.github.com>
2023-02-04 01:17:56 -0800

    cleaned up notes

--- download_script.py
+++ download_script.py
@@ -53,7 +53,10 @@ for filename in os.listdir(downloads_folder):
 
 print("Downloads folder organized.")
 
-# run these commands in terminal of host computer
-# sudo python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
-# python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
-# chmod +x /Users/home/Downloads
+"""
+run these commands in terminal of host computer
+sudo python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
+python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
+chmod +x /Users/home/Downloads
+"""
+
