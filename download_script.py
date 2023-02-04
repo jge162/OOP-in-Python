d1dc0d5d4c72532288308f124936407fdd1eddd5
Jermyiah <31228460+jge162@users.noreply.github.com>
2023-02-04 00:37:10 -0800

    python script to organize downloads folder

--- download_script.py
+++ download_script.py
@@ -44,4 +44,5 @@ for filename in os.listdir(downloads_folder):
 
 print("Downloads folder organized.")
 
-# python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
+# sudo python3 /Users/home/Documents/GitHub/OOP_in_Python/download_script.py
+# chmod +x /Users/home/Downloads
